3913133bc00e4e1da19254fe03cefd2914838edd
integerJI <jjs9536@gmail.com>
2019-11-13 20:25:22 +0900

    q

--- qrproject/settings.py
+++ qrproject/settings.py
@@ -133,6 +133,16 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'postapp', 'static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
+
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
--- qrproject/urls.py
+++ qrproject/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from qr_code import urls as qr_code_urls
+from django.conf import settings
+from django.conf.urls.static import static
 from qrapp import views
 
 urlpatterns = [
@@ -8,3 +10,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('qr_code/', include(qr_code_urls, namespace='qr_code')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
